b7354fa55ca8e06b20e05ed695fb934779bfa69e
maletiger <maletiger000@gmail.com>
2021-04-22 12:09:52 +0300

    Router

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const config = require('./db');
 const app = express();
 const server = require("http").Server(app);
-const adminRouter = require("./router");
+const adminRouter = require("./adminRouter");
+const playerRouter = require("./playerRouter");
 const path = require("path");
 
 mongoose.connect(config.TESTDB, { useNewUrlParser: true ,useFindAndModify: false,useUnifiedTopology: true,useCreateIndex : true}).then(() => {
@@ -16,7 +17,7 @@ mongoose.connect(config.TESTDB, { useNewUrlParser: true ,useFindAndModify: false
   app.use(bodyParser.json({limit: "15360mb", type:'application/json'}));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/admin', adminRouter);
-  // app.use('/player', playerRouter)
+  app.use('/player', playerRouter)
     
   app.get('*', (req, res) => {
     res.sendFile(path.join(config.DIR, 'clients/builds/index.html'));  
--- playerRouter/index.js
+++ playerRouter/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const DashboardRouter = require("./dashboard_router");
+const userRouter = require("./user_router")
 
 router.use("/dashboard",DashboardRouter);
+router.use("/user",userRouter);
+
 module.exports = router;
