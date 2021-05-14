566b33cf784d344a6a23d28a545fc7fb0cbebb1f
maletiger <maletiger000@gmail.com>
2021-05-14 21:41:49 +1000

    Update AR to dynamic

--- clients/places.js
+++ clients/places.js
@@ -1,74 +1,38 @@
 const serverURL = "https://vr.myyaak.com/"
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const session_token = urlParams.get("session_token")
+const username = urlParams.get("username")
+const password = urlParams.get("password")
+
 const loadPlaces = function(coords) {
     // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
     //const method = 'api';
 
-    const PLACES = [
-        {
-            name: "MAZA BAR",
-            location: {
-                lat: -27.843834, // add here latitude if using static data
-                lng: 153.340407, // add here longitude if using static data
-            },
-            img: "https://cdn.glitch.com/3aae3d53-f072-40dc-8f63-7cb561c70c89%2F02.jpg?v=1578314395239",
-            
-        },
+    const PLACES = [];
 
-    ];
-  
-
-    //if (method === 'api') {
-    return loadPlaceFromAPIs(coords);
-    //}
-
-    // return Promise.resolve(PLACES);
-};
-
-// getting places from REST APIs
-function loadPlaceFromAPIs(position) {
-    // const params = {
-    //     radius: 300,    // search places not farther than this value (in meters)
-    //     clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
-    //     clientSecret: '',
-    //     version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    // };
-
-    // // CORS Proxy to avoid CORS problems
-    // const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // // Foursquare API
-    // const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-    //     &ll=${position.latitude},${position.longitude}
-    //     &radius=${params.radius}
-    //     &client_id=${params.clientId}
-    //     &client_secret=${params.clientSecret}
-    //     &limit=15
-    //     &v=${params.version}`;
-    return fetch(`${serverURL}player/tasks/getTasks`, {
+    const response = await fetch(`${serverURL}player/tasks/getTasks`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'User': JSON.stringify({token: { session_token }}),
             'LogIn': JSON.stringify({username, password})
         }
-    }).then((res) => {
-        return res.json()
-            .then((resp) => {
-                if (resp.session == true) {
-                    window.location.href = "./not-authrized.html"
-                }
-                if (resp.status == true) {
-                    return resp.data
-                } else {
-                    return []
-                }
-            })
     })
-    .catch((err) => {
-        console.error('Error with places API', err);
-    })
+
+    const resultData = await response.json()
+    console.log(`resultData`, resultData)
+    if (resultData.session) {
+        window.location.href = "./not-authrized.html"
+    }
+    if (resultData.status == true) {
+        PLACES = resultData.data
+    }
+
+    return Promise.resolve(PLACES);
 };
+
 
 let latitude;
 let longitude;
@@ -80,10 +44,6 @@ var ler = true;
 window.onload = () => {
     const scene = document.querySelector('a-scene');
 
-    const parent_assets = document.createElement("a-assets")
-
-    scene.appendChild(parent_assets)
-
     // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
 
@@ -91,50 +51,50 @@ window.onload = () => {
       
         loadPlaces(position.coords)
             .then((places) => {
-                console.log(`places`, places)
                 places.forEach((place) => {
-                    const { file_type } = place
+                    
                     if(ler){
                         latitude = place.task_position.lat;
                         longitude = place.task_position.lng;
                     }
                     // add place icon
-                    if (file_type == "image") {
-                        const icon = document.createElement('a-image');
-                        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                        icon.setAttribute('name', place.task_name);
-                        icon.setAttribute('src', `${serverURL}${place.task_file}`);
-                        icon.setAttribute("look-at", "[camera]");
-    
-                        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                        icon.setAttribute('scale', '10, 10');
-    
-                        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-                        
-                        scene.appendChild(icon);
-                    } else if (file_type == "video") {
-                        const assets = document.querySelector('a-assets')
-                        
-                        const video = document.createElement("video");
-                        video.setAttribute("id", place._id)
-                        video.setAttribute("autoplay", "true")
-                        video.setAttribute("loop", "true")
-                        video.setAttribute("src", `${serverURL}${place.task_file}`)
+                    const icon = document.createElement('a-image');
+                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                    icon.setAttribute('name', place.task_name);
+                    //icon.setAttribute('src', '../assets/map-marker.png');
+                    icon.setAttribute('src', `${serverURL}${place.task_file}`);
+                    icon.setAttribute("look-at","[camera]");
 
-                        assets.appendChild(video)
+                    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+                    icon.setAttribute('scale', '20, 20');
 
-                        const avideo = document.createElement('a-video');
-                        avideo.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                        avideo.setAttribute('src', `#${place._id}`);
-                        avideo.setAttribute("look-at", "[camera]");
-    
-                        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                        avideo.setAttribute('scale', '10, 10');
-    
-                        avideo.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-                        
-                        scene.appendChild(avideo);
-                    }
+                    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+                    const clickListener = function(ev) {
+                        ev.stopPropagation();
+                        ev.preventDefault();
+
+                        const name = ev.target.getAttribute('name');
+
+                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+                        if (el && el === ev.target) {
+                            const label = document.createElement('span');
+                            const container = document.createElement('div');
+                            container.setAttribute('id', 'place-label');
+                            label.innerText = name;
+                            container.appendChild(label);
+                            document.body.appendChild(container);
+
+                            setTimeout(() => {
+                                container.parentElement.removeChild(container);
+                            }, 1500);
+                        }
+                    };
+
+                    //icon.addEventListener('click', clickListener);
+                    
+                    scene.appendChild(icon);
                   
                   
                     // add place name
